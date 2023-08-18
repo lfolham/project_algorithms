ffdec359077ef645c31eec170a525615390ff4e5
lfolham <lucasfolha@icloud.com>
2023-08-18 17:13:41 -0300

    task 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,35 +1,13 @@
 def study_schedule(permanence_period, target_time):
-
-    if not permanence_period or target_time is None:
+    if target_time is None or not all(
+        len(period) == 2 and isinstance(period[0], int) and
+        isinstance(period[1], int) and period[0] <= period[1]
+        for period in permanence_period
+    ):
         return None
 
-    count = 0
-
-    for period in permanence_period:
-        if len(period) != 2 or period[0] > period[1]:
-            return None
-
-        if period[0] <= target_time <= period[1]:
-            count += 1
-
+    count = sum(
+        period[0] <= target_time <= period[1]
+        for period in permanence_period
+    )
     return count
-
-
-permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
-target_times = [5, 4, 3, 2, 1]
-
-max_count = 0
-best_time = None
-
-for time in target_times:
-    count = study_schedule(permanence_period, time)
-    if count is None:
-        print("Permanence period or target time is invalid.")
-        break
-
-    if count > max_count:
-        max_count = count
-        best_time = time
-
-if best_time is not None:
-    print(f"O melhor horário é {best_time}, com {max_count} presentes.")
