f1ba4ed94873c782ad5b2f43a61672572cc4c471
lfolham <lucasfolha@icloud.com>
2023-08-18 18:07:15 -0300

    task 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,15 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message('AABBCC', 3) == 'BAA_CCB'
+    assert encrypt_message('ABBCCA', 4) == 'AC_CBBA'
+    assert encrypt_message('ABC', 4) == 'CBA'
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message('AABBCC', '3')
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(123456, 3)
+    assert encrypt_message('message', 9) == 'egassem'
+    assert encrypt_message('amessage', 4) == 'egas_sema'
+    assert encrypt_message('message', 3) == 'sem_egas'
